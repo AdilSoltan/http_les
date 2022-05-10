ab1493ada1abb8746c5ae30c170a5f1d991ba7cc
Soltan Adil <adil01king@mail.ru>
2022-05-11 01:59:19 +0600

    Full search

---
+++ get_delta.py
@@ -0,0 +1,10 @@
+
+
+def get_param(toponym):
+    print(toponym["boundedBy"]["Envelope"])
+    toponym_size = [[float(j) for j in i.split()] for i in
+                    list(toponym["boundedBy"]["Envelope"].values())]
+
+    delta = str(min(abs(toponym_size[0][0] - toponym_size[1][0]),
+                    abs(toponym_size[0][1] - toponym_size[1][1])))
+    return delta
---
+++ search.py
@@ -0,0 +1,52 @@
+import sys
+from io import BytesIO
+# Этот класс поможет нам сделать картинку из потока байт
+
+import requests
+from PIL import Image
+from get_delta import get_param
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    print('Nothing found')
+    sys.exit(0)
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+# Координаты центра топонима:
+toponym_coodrinates = toponym["Point"]["pos"]
+# Долгота и широта:
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+delta = get_param(toponym)
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map",
+    "pt": f"{','.join([toponym_longitude, toponym_lattitude])},pm2dgl"
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+
+Image.open(BytesIO(
+    response.content)).show()
